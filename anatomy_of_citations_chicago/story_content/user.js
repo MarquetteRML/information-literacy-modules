35f19e286bc93675494e8208e5fdd62e32452d7e
kowalike <eric.kowalik@marquette.edu>
2017-04-19 11:40:02 -0500

    Added Chicago Style Tutorial
    - Added Chicago Style tutorial
    - Updated index.html to link to Chicago Style tutorial and also added
    prose in the first paragraph linking to the LOEX paper co-authored with
    GVSU.

---
+++ anatomy_of_citations_chicago/story_content/user.js
@@ -0,0 +1,169 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68s8p0z91qr":
+        Script1();
+        break;
+      case "5fggIEKNYWP":
+        Script2();
+        break;
+      case "64Zh6ZMQEjS":
+        Script3();
+        break;
+      case "62cW7FHaqek":
+        Script4();
+        break;
+      case "62L3w0t0BEi":
+        Script5();
+        break;
+      case "5wlZ8vq7H7Z":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+}
+
+function Script2()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+}
+
+function Script3()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+
+
+
+
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script4()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+}
+
+function Script5()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+}
+
+function Script6()
+{
+  var months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December"
+
+var currentTime = new Date()
+var month = currentTime.getMonth()
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=  months[month] + " " + day + ", " + year
+}
+
