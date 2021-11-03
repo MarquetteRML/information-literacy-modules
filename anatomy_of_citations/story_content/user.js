28b9784e048793b65d3c6a024e37b2f1dfd241d7
Eric Kowalik <eric.kowalik@marquette.edu>
2021-11-03 13:30:28 -0500

    Accessibility Update
    - Enhanced: Removed Home and Back buttons from graded slides.
    - Enhanced: Implemented key board accessible drag and drop.
    - Enhanced: Improved tab order and alternative text for assistive technology users.
    - Enhanced: Updated visited state alternative text so assistive technology will know it was visited.
    - New: Utilized Javascript to create a random variable to select which of the practice source sets user will see.

--- anatomy_of_citations/story_content/user.js
+++ anatomy_of_citations/story_content/user.js
@@ -1,1 +1,16 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6J7GhuBiRSn":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var number=Math.floor(Math.random()*10);
+GetPlayer().SetVar("RandNum",number);
+}
+
