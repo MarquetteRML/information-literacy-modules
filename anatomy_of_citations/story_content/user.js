0289064bbc5ae8367639380f0090003e73e52369
kowalike <eric.kowalik@marquette.edu>
2024-05-28 15:47:12 -0500

    Fixed Score Not Sending
    - Fixed: Updated to the Modern Player so score submission slides no longer freeze before reaching final slide with student score.

--- anatomy_of_citations/story_content/user.js
+++ anatomy_of_citations/story_content/user.js
@@ -2,15 +2,23 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6J7GhuBiRSn":
+      case "5cGruT4Tvbn":
         Script1();
         break;
   }
 }
 
-function Script1()
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
 {
   var number=Math.floor(Math.random()*10);
 GetPlayer().SetVar("RandNum",number);
 }
 
+};
