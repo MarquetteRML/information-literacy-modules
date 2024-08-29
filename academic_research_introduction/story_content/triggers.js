05c3ddb1d235efce3939d7c47da9857b73797948
kowalike <eric.kowalik@marquette.edu>
2024-08-29 11:01:57 -0500

    Removed screencast video
    - Fixed: Removed outdated screencast video on how to search Academic Search Complete.
    - Enhanced: Updated to the Articulate Modern Player.

---
+++ academic_research_introduction/story_content/triggers.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ academic_research_introduction/story_content/user.js
@@ -0,0 +1,8 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
